0bbe36d776312af2e489e0a903d6c7c4674e6a20
Sergio Socas <sergitosoc737@gmail.com>
2026-01-11 22:59:47 +0000

    Version 1.0 terminada

--- app.py
+++ app.py
@@ -5,20 +5,25 @@ opcion = f.validar_entrada_rango(1,3,"Introduzca una opción dado el siguiente m
 while opcion != 3:
     if opcion == 1:
         print("Seleccione un modo de juego: \n1.Jugador vs jugador\n2.Jugador vs maquina")
-        opcion2 = f.validar_entrada_rango(1,2,"Introduzca un modo de juego")
+        opcion2 = f.validar_entrada_rango(1,2,"Introduzca un modo de juego: ")
         if opcion2 == 1:
             condicion = True
             i = 0
             tablero = f.crear_tablero()
+            f.imprimir_tablero(tablero)
             while condicion:
+                tablero_ant = f.copia_tablero(tablero)
                 if i%2 == 0: color = "R"
                 else: color = "B"
                 ult_ficha = f.insertar_ficha(tablero,color)
+                if tablero_ant == tablero:
+                    print("Introduccion invalida, intentelo de nuevo: ")
+                    continue
                 ganadorc = f.combrobar_ganador(tablero,ult_ficha[0],ult_ficha[1])
                 if ganadorc[0]:
-                    condicion=False
+                    break
                 if f.combrobar_tablerolleno(tablero):
-                    condicion = False
+                    break
                 f.imprimir_tablero(tablero)
                 i+=1
             if ganadorc[1] == ("[""\033[41m"+"  "+"\033[0m""]"): colorg = "Rojo"
@@ -46,5 +51,50 @@ while opcion != 3:
             condicion = True
             i = 0
             tablero = f.crear_tablero()
-            
+            f.imprimir_tablero(tablero)
+            while condicion:
+                tablero_ant = f.copia_tablero(tablero)
+                ult_ficha = f.insertar_ficha(tablero,"R")
+                if tablero == tablero_ant:
+                    print("Introduccion invalida, repita la entrada")
+                    continue
+                i +=1
+                ganadorc = f.combrobar_ganador(tablero,ult_ficha[0],ult_ficha[1])
+                if ganadorc[0]:
+                    break
+                ult_ficha = f.algoritmo_maquina(tablero)
+                f.imprimir_tablero(tablero)
+                i+=1
+                ganadorc = f.combrobar_ganador(tablero,ult_ficha[0],ult_ficha[1])
+                if ganadorc[0]:
+                    break
+                if f.combrobar_tablerolleno(tablero):
+                    condicion = False
+            if i%2 != 0: colorg = "Rojo"
+            elif i%2 == 0: colorg = "Azul"
+            if i <42:
+                if colorg == "Rojo":
+                    print(f"El ganador ha sido el jugador {colorg}")
+                    nombreg = input("Introduzca el nombre del ganador: ")
+                    if nombreg not in estadisticas:
+                        f.crear_estadisticas_jugador(estadisticas,nombreg)
+                    f.guardar_estadisticas_ganado(estadisticas,(i//2+i%2),nombreg)
+                    f.guardar_estadisticas_perdido(estadisticas,"Maquina")
+                elif colorg == "Azul":
+                    print(f"El ganador ha sido {colorg}")
+                    f.guardar_estadisticas_ganado(estadisticas,(i//2+i%2),"Maquina")
+                    nombrep= input("Introduzca el nombre del perdedor: ")
+                    if nombrep not in estadisticas:
+                        f.crear_estadisticas_jugador(estadisticas,nombreg)
+                    f.guardar_estadisticas_perdido(estadisticas,nombrep)
+            else:
+                print("Empatado!")
+                nombre = input("Introduzca su nombre: ")
+                if nombre not in estadisticas:
+                    f.crear_estadisticas_jugador(estadisticas,nombre)
+                f.guardar_estadisticas_empatado(estadisticas,nombre)
+                f.guardar_estadisticas_empatado(estadisticas,"Maquina")
 print("Finalizando...")
+print("Mostrando estadisticas:\n")
+print(estadisticas)
+
--- funciones_basicas.py
+++ funciones_basicas.py
@@ -64,6 +64,31 @@ def insertar_ficha(tablero:list,color:str)->tuple:
             else:
                 i-=1
         #En el programa principal comparar tableros, si no ha cambiado nada repetir la orden
+def insertar_ficha_segunx(tablero:list,color:str,x:int)->tuple:    
+    """
+    OBJ: Insertar una ficha de un color determinado en una columna determinada
+    PRE: Color ("R","B")
+    """
+    Rojo = ("[""\033[41m"+"  "+"\033[0m""]")
+    Azul = ("[""\033[44m"+"  "+"\033[0m""]")
+    Vacio = "[  ]"
+    if color == "R":
+        i =len(tablero)-1
+        while i>=0:
+            if tablero[i][x-1]==Vacio:
+                tablero[i][x-1] = Rojo
+                return i,(x-1)
+            else:
+                i-=1     
+    elif color == "B":
+        i =len(tablero)-1
+        while i>=0:
+            if tablero[i][x-1] == Vacio and i>=0:
+                tablero[i][x-1] = Azul
+                return i,(x-1)
+            else:
+                i-=1
+    return None
 def imprimir_tablero(tablero:list)->None:
     """
     OBJ: Imprimir el tablero de manera que se represente en forma de matriz
@@ -87,24 +112,32 @@ def combrobar_ganador(tablero:list,x:int,y:int)->bool:
     OBJ: Comrpobar si la ultima ficha insertada hace 4 en raya
     PRE: Dados los parametros devueltos de la funcion insertar ficha
     """
-    mov = [(1,0),(-1,0),(0,1),(0,-1),(1,1),(-1,-1),(1,-1),(-1,1)]
-    for movimiento in mov:
-        i = 1
-        condicion = True
-        while condicion:
-            x2,y2 = x+ i*movimiento[0],y+ i*movimiento[1]
-            if 0<=x2<=5 and 0<=y2<=6:
-                if tablero[x][y] != tablero[x2][y2]:
-                    condicion = False
+    ejes= [
+        [(1,0),(-1,0)],
+        [(0,1),(0,-1)],
+        [(1,1),(-1,-1)],
+        [(1,-1),(-1,1)]
+        ]
+    for eje in ejes:
+        contador = 1
+        for (dirx,diry) in eje:
+            i = 1
+            condicion = True
+            while condicion:
+                x2,y2 = x+ i*dirx,y+ i*diry
+                if 0<=x2<=5 and 0<=y2<=6:
+                    if tablero[x][y] != tablero[x2][y2]:
+                        condicion= False
+                    else:
+                        i+=1
+                        contador+=1
                 else:
-                    i+=1
-            else:
-                condicion = False
-        if i == 4:
-            return True,tablero[x][y]
-    return False
+                    condicion = False
+            if contador >= 4:
+                return True,tablero[x][y]
+    return False,"Vacio"
 def crear_estadisticas_jugador(estadisticas:dict,nombre:str):
-    estadisticas[nombre] = {"Ganados":0,"Empatados":0,"Perdidos":0,"Puntuacion":0}
+    estadisticas[nombre] = {"Ganado":0,"Empatado":0,"Perdido":0,"Puntuacion":0}
 def guardar_estadisticas_ganado(estadisticas:dict,rondas:float,nombre:str)->None:
     # Usaremos dump |Python->Json|, load |Json->Python|
     # estadisticas = {nombre:{ganados:0,empatados:0,perdidos:0,puntuacion:0}nombre2:{.....}}
@@ -132,68 +165,191 @@ def fork_supuesto (tablero:list,x:int,y:int)->bool:
     OBJ: Comrpobar si la ultima ficha insertada hace 2 3 en raya
     PRE: Dados los parametros devueltos de la funcion insertar ficha
     """
-    mov = [(1,0),(-1,0),(0,1),(0,-1),(1,1),(-1,-1),(1,-1),(-1,1)]
-    contador = 0
-    for movimiento in mov:
-        i = 1
-        condicion = True
-        while condicion:
-            x2,y2 = x+ i*movimiento[0],y+ i*movimiento[1]
-            if 0<=x2<=5 and 0<=y2<=6:
-                if tablero[x][y] != tablero[x2][y2]:
-                    condicion = False
+    ejes= [
+        [(1,0),(-1,0)],
+        [(0,1),(0,-1)],
+        [(1,1),(-1,-1)],
+        [(1,-1),(-1,1)]
+        ]
+    sub_contador=0
+    for eje in ejes:
+        contador = 1
+        for (dirx,diry) in eje:
+            i = 1
+            condicion = True
+            while condicion:
+                x2,y2 = x+ i*dirx,y+ i*diry
+                if 0<=x2<=5 and 0<=y2<=6:
+                    if tablero[x][y] != tablero[x2][y2]:
+                        condicion= False
+                    else:
+                        i+=1
+                        contador+=1
                 else:
-                    i+=1
-            else:
-                condicion = False
-        if i == 4:
-            contador +=1
-    if contador == 2:
-        return True, tablero[x][y]
-    return False
+                    condicion = False
+            if contador == 3:
+                sub_contador+=1
+            if sub_contador >=2:
+                return True,tablero[x][y]
+    return False,"Vacio"
 #La posicion central deberia de ir en la funcion principal del algortimo
 def conectar3(tablero:list,x:int,y:int)->bool:
     """
     OBJ: Comrpobar si la ultima ficha insertada hace 3 en raya
     PRE: Dados los parametros devueltos de la funcion insertar ficha
     """
-    mov = [(1,0),(-1,0),(0,1),(0,-1),(1,1),(-1,-1),(1,-1),(-1,1)]
-    for movimiento in mov:
-        i = 1
-        condicion = True
-        while condicion:
-            x2,y2 = x+ i*movimiento[0],y+ i*movimiento[1]
-            if 0<=x2<=5 and 0<=y2<=6:
-                if tablero[x][y] != tablero[x2][y2]:
-                    condicion = False
+    ejes= [
+        [(1,0),(-1,0)],
+        [(0,1),(0,-1)],
+        [(1,1),(-1,-1)],
+        [(1,-1),(-1,1)]
+        ]
+    for eje in ejes:
+        contador = 1
+        for (dirx,diry) in eje:
+            i = 1
+            condicion = True
+            while condicion:
+                x2,y2 = x+ i*dirx,y+ i*diry
+                if 0<=x2<=5 and 0<=y2<=6:
+                    if tablero[x][y] != tablero[x2][y2]:
+                        condicion= False
+                    else:
+                        i+=1
+                        contador+=1
                 else:
-                    i+=1
-            else:
-                condicion = False
-        if i == 3:
-            return True,tablero[x][y]
-    return False
+                    condicion = False
+            if contador == 3:
+                return True,tablero[x][y]
+    return False,"Vacio"
 def conectar2(tablero:list,x:int,y:int)->bool:
     """
     OBJ: Comrpobar si la ultima ficha insertada hace 2 en raya
     PRE: Dados los parametros devueltos de la funcion insertar ficha
     """
-    mov = [(1,0),(-1,0),(0,1),(0,-1),(1,1),(-1,-1),(1,-1),(-1,1)]
-    for movimiento in mov:
-        i = 1
-        condicion = True
-        while condicion:
-            x2,y2 = x+ i*movimiento[0],y+ i*movimiento[1]
-            if 0<=x2<=5 and 0<=y2<=6:
-                if tablero[x][y] != tablero[x2][y2]:
-                    condicion = False
+    ejes= [
+        [(1,0),(-1,0)],
+        [(0,1),(0,-1)],
+        [(1,1),(-1,-1)],
+        [(1,-1),(-1,1)]
+        ]
+    for eje in ejes:
+        contador = 1
+        for (dirx,diry) in eje:
+            i = 1
+            condicion = True
+            while condicion:
+                x2,y2 = x+ i*dirx,y+ i*diry
+                if 0<=x2<=5 and 0<=y2<=6:
+                    if tablero[x][y] != tablero[x2][y2]:
+                        condicion= False
+                    else:
+                        i+=1
+                        contador+=1
                 else:
-                    i+=1
-            else:
-                condicion = False
-        if i == 2:
-            return True,tablero[x][y]
-    return False
+                    condicion = False
+            if contador == 2:
+                return True,tablero[x][y]
+    return False,"Vacio"
 
 def algoritmo_maquina(tablero:list)->tuple:
-    mapa_calor= 
+    mapa_calor= [
+        [30,40,50,70,50,40,30],
+        [40,60,80,100,80,60,40],
+        [50,80,110,130,110,80,50],
+        [50,80,110,130,110,80,50],
+        [40,60,80,100,80,60,40],
+        [30,40,50,70,50,40,30]
+    ]
+    Vacio = "[  ]"
+    puntuacion_mayor = 0
+    posicion  = -9999999999999999999
+    for i in range(7):
+        tablero_aux = copia_tablero(tablero)
+        ficha_ins = insertar_ficha_segunx(tablero_aux,"B",i+1) #El mas 1 porque la funcion utiliza x-1 para que cuando el usuario meta un 7 vaya a la ultima al ser las columnas (0,6)
+        puntuacion_actual = 0
+        if ficha_ins is None:
+            continue
+        #Si una fila ya esta llena no la comprobamos
+        if combrobar_ganador(tablero_aux,ficha_ins[0],ficha_ins[1])[0]:
+            puntuacion_actual = 10000
+        else:
+            tablero_aux[ficha_ins[0]][ficha_ins[1]] = Vacio
+            ficha_ins = insertar_ficha_segunx(tablero_aux,"R",i+1)
+            if combrobar_ganador(tablero_aux,ficha_ins[0],ficha_ins[1])[0]:
+                puntuacion_actual= 9000
+            else:
+                tablero_aux[ficha_ins[0]][ficha_ins[1]] = Vacio
+                ficha_ins = insertar_ficha_segunx(tablero_aux,"B",i+1)
+                if fork_supuesto(tablero_aux,ficha_ins[0],ficha_ins[1])[0]:
+                    puntuacion_actual = 8000
+                else:
+                    tablero_aux[ficha_ins[0]][ficha_ins[1]] = Vacio
+                    ficha_ins = insertar_ficha_segunx(tablero_aux,"R",i+1)
+                    if fork_supuesto(tablero_aux,ficha_ins[0],ficha_ins[1])[0]:
+                        puntuacion_actual= 7000
+                    else:
+                        tablero_aux[ficha_ins[0]][ficha_ins[1]] = Vacio
+                        ficha_ins = insertar_ficha_segunx(tablero_aux,"R",i+1)
+                        if conectar3(tablero_aux,ficha_ins[0],ficha_ins[1])[0]:
+                            puntuacion_actual= 6500
+                        else:
+                            tablero_aux[ficha_ins[0]][ficha_ins[1]] = Vacio
+                            ficha_ins = insertar_ficha_segunx(tablero_aux,"B",i+1)
+                            if conectar3(tablero_aux,ficha_ins[0],ficha_ins[1])[0]:
+                                puntuacion_actual = 150
+                            else:
+                                tablero_aux[ficha_ins[0]][ficha_ins[1]] = Vacio
+                                ficha_ins = insertar_ficha_segunx(tablero_aux,"B",i+1)
+                                if conectar2(tablero_aux,ficha_ins[0],ficha_ins[1])[0]:
+                                    puntuacion_actual = 100
+                                else:
+                                    tablero_aux[ficha_ins[0]][ficha_ins[1]] = Vacio
+                                    ficha_ins = insertar_ficha_segunx(tablero_aux,"B",i+1)
+                                    if puntuacion_actual<mapa_calor[ficha_ins[0]][ficha_ins[1]]:
+                                        puntuacion_actual = mapa_calor[ficha_ins[0]][ficha_ins[1]]
+        tablero_aux[ficha_ins[0]][ficha_ins[1]] = Vacio
+        ficha_ins = insertar_ficha_segunx(tablero_aux,"B",i+1)
+        if comprobar_columna_llena1(tablero_aux,i) and puntuacion_actual<5000:
+                puntuacion_actual += consecuencias_rival(tablero_aux,i+1)
+        if puntuacion_actual > puntuacion_mayor: puntuacion_mayor,posicion = puntuacion_actual,i
+    ficha_final = insertar_ficha_segunx(tablero,"B",posicion+1)
+    return ficha_final
+
+def consecuencias_rival(tablero:list,x:int)->int:
+    """
+    En tablero siempre pondre el auxiliar, y comprobaremos que pasa si el jugador inserta una ficha en dicha posicion
+    Nos devuelve la puntuacion
+    """
+    tablero_prueba = copia_tablero(tablero)
+    ficha = insertar_ficha_segunx(tablero_prueba,"R",x)
+    if conectar3(tablero_prueba,ficha[0],ficha[1])[0] or conectar2(tablero_prueba,ficha[0],ficha[1])[0] or combrobar_ganador(tablero_prueba,ficha[0],ficha[1])[0] or fork_supuesto(tablero_prueba,ficha[0],ficha[1])[0]:
+        puntuacion = -1000000
+    else:
+        puntuacion= 0
+    return puntuacion
+     
+def copia_tablero(tablero:list)->list:
+    """
+    Copiamos el tablero y lo asignamos a una variable nueva
+    """
+    tablero_nuevo = []
+    for i in tablero:
+        fila = []
+        for j in range(len(i)):
+            fila.append( i[j])
+        tablero_nuevo.append(fila)
+    return tablero_nuevo
+def comprobar_columna_llena1(tablero:list,x:int)->bool:
+    """
+    OBJ: Devuelve True si todavia se pueden poner fichas
+    """
+    Vacio = "[  ]"
+    contador = 0
+    for i in range(6):
+        if tablero[i][x] == Vacio:
+            contador+=1
+    if contador >=1: #Esto es 2 porque queremos ver que pasaria con la ficha que el jugador pone despues de la nuestra
+        return True
+    else:
+        return False
